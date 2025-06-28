815dd12d1e5ee482e293504114426a00111b5a64
Dr4gonX <oiranzinho00@gmail.com>
2025-06-28 16:50:55 -0300

    D4gonHub.lua

--- D4gonHub.lua
+++ D4gonHub.lua
@@ -19,4 +19,60 @@ Tab:AddButton({
   	end    
 })
 
+Tab:AddButton({
+	Name = "Beamed V2 - benishot",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/ben1x1x1x1x/V2/refs/heads/main/BeamedV2FreeLoader'))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "KTM - KTMx2025",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/zapstreams123/KTMHUB/refs/heads/main/PublicVersion'))()
+  	end    
+})
+
+-- ABA BLOX FRUITS
+
+local BFTab = Window:MakeTab({
+	Name = "Blox Fruits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local TwoSection = BFTab:AddSection({
+	Name = "Auto Farm"
+})
+
+BFTab:AddButton({
+	Name = "Speed Hub X",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua'))()
+  	end    
+})
+
+BFTab:AddButton({
+	Name = "Redz Hub",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua'))()
+  	end    
+})
+
+BFTab:AddButton({
+	Name = "Cokka Hub",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua'))()
+  	end    
+})
+
+BFTab:AddButton({
+	Name = "Achelmy Hub",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/DENGHUB2025/HUGHUB/main/WL'))()
+  	end    
+})
+
+
+
 OrionLib:Init()
